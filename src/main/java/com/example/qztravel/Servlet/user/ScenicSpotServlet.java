d4710d5f2f6cdf222aee47c45262aa8ff6355824
mianxianhu <2638521895@qq.com>
2025-01-03 21:45:53 +0800

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/example/qztravel/Servlet/user/ScenicSpotServlet.java
+++ src/main/java/com/example/qztravel/Servlet/user/ScenicSpotServlet.java
@@ -4,50 +4,30 @@ import com.example.qztravel.Bean.ScenicSpotBean;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+@WebServlet("/ScenicSpot")
 public class ScenicSpotServlet extends HttpServlet {
+    private static final Logger logger = Logger.getLogger(ScenicSpotServlet.class.getName());
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         ScenicSpotDao dao = new ScenicSpotDao();
         List<ScenicSpotBean> scenicSpots = dao.getAllScenicSpots();
-
-        // 将景区信息列表存储在请求中
+        logger.log(Level.INFO,"ScenicSpots size before setting attribute: " + (scenicSpots == null? 0 : scenicSpots.size()));
         request.setAttribute("scenicSpots", scenicSpots);
-
-        // 转发到 JSP 页面
         request.getRequestDispatcher("/scenicSpot.jsp").forward(request, response);
     }
 
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String action = request.getParameter("action");
-        if ("add".equals(action)) {
-            ScenicSpotBean scenicSpot = new ScenicSpotBean();
-            scenicSpot.setName(request.getParameter("name"));
-            scenicSpot.setDescription(request.getParameter("description"));
-            scenicSpot.setAddress(request.getParameter("address"));
-            scenicSpot.setOpenTime(request.getParameter("openTime"));
-            ScenicSpotDao dao = new ScenicSpotDao();
-            dao.addScenicSpot(scenicSpot);
-        } else if ("update".equals(action)) {
-            ScenicSpotBean scenicSpot = new ScenicSpotBean();
-            scenicSpot.setName(request.getParameter("name"));
-            scenicSpot.setDescription(request.getParameter("description"));
-            scenicSpot.setAddress(request.getParameter("address"));
-            scenicSpot.setOpenTime(request.getParameter("openTime"));
-            ScenicSpotDao dao = new ScenicSpotDao();
-            dao.updateScenicSpot(scenicSpot);
-        } else if ("delete".equals(action)) {
-            String name = request.getParameter("name");
-            ScenicSpotDao dao = new ScenicSpotDao();
-            dao.deleteScenicSpot(name);
-        }
         doGet(request, response);
+
     }
 }
--- src/main/webapp/scenicSpot.jsp
+++ src/main/webapp/scenicSpot.jsp
@@ -1,16 +1,38 @@
-<!-- scenicSpot.jsp -->
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page import="com.example.qztravel.Bean.ScenicSpotBean" %>
+<%@ page import="java.util.List" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+         pageEncoding="UTF-8"%>
+<!DOCTYPE html>
 <html>
 <head>
+  <meta charset="UTF-8">
   <title>景区介绍</title>
 </head>
 <body>
 <h1>景区介绍</h1>
-<div>
-  <h2>${scenicSpot.getName()}</h2>
-  <p>${scenicSpot.getDescription()}</p>
-  <p>地址：${scenicSpot.getAddress()}</p>
-  <p>开放时间：${scenicSpot.getOpenTime()}</p>
-</div>
+<table border="1" >
+  <tr>
+    <th>名称</th>
+    <th>描述</th>
+    <th>地址</th>
+    <th>开放时间</th>
+  </tr>
+  <%
+    // 从请求中获取景区信息列表
+    List<ScenicSpotBean> scenicSpots = (List<ScenicSpotBean>) request.getAttribute("scenicSpots");
+    if (scenicSpots!= null) {
+      for (ScenicSpotBean scenicSpot : scenicSpots) {
+  %>
+  <tr >
+    <td><%= scenicSpot.getName() %></td>
+    <td><%= scenicSpot.getDescription() %></td>
+    <td><%= scenicSpot.getAddress() %></td>
+    <td><%= scenicSpot.getOpenTime() %></td>
+  </tr>
+  <%
+      }
+    }
+  %>
+</table>
 </body>
 </html>
